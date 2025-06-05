96c9faf1813262f5918f8e293913203996bd6063
GlebKostousov <kostousjr@gmail.com>
2025-06-05 19:23:08 +0300

    Добавил все свои правила  в ruff:

--- url-shortener/api/api_v1/auth/services/redis_users_helper.py
+++ url-shortener/api/api_v1/auth/services/redis_users_helper.py
@@ -33,7 +33,7 @@ class UserTokensHelper(AbstractUsersHelper):
         if not self.redis_users.exists(username):
             return None
 
-        return cast(str | None, self.redis_users.get(name=username))
+        return cast("str | None", self.redis_users.get(name=username))
 
     def add_user(self, username: str, password: str) -> None:
         self.redis_users.set(name=username, value=password)
