5cf4b31baca7fe4b83c945972ff2873ddcb3055c
GlebKostousov <kostousjr@gmail.com>
2025-06-18 16:39:55 +0300

    Написал тесты для проверки get_by_slug и get_list

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
@@ -1,6 +1,7 @@
 import random
 import string
 from os import getenv
+from typing import ClassVar, Final
 from unittest import TestCase
 
 from pydantic import AnyHttpUrl
@@ -18,26 +19,22 @@ if getenv("TESTING") != "1":
     raise OSError(error_testing_msg)
 
 
-def total(a: int, b: int) -> int:
-    return a + b
+def create_short_url() -> ShortUrl:
+    short_url_in = ShortUrlCreate(
+        target_url=AnyHttpUrl("https://google.com"),
+        description="dsada",
+        slug="".join(random.choices(string.ascii_letters, k=8)),
+    )
+    return storage.create(short_url_in)
 
 
 class ShortUrlStorageUpdateTestCase(TestCase):
 
     def setUp(self) -> None:
-        self.short_url = self.create_short_url()
+        self.short_url = create_short_url()
 
     def tearDown(self) -> None:
         storage.delete(self.short_url)
-
-    @classmethod
-    def create_short_url(cls) -> ShortUrl:
-        short_url_in = ShortUrlCreate(
-            target_url=AnyHttpUrl("https://google.com"),
-            description="dsada",
-            slug="".join(random.choices(string.ascii_letters, k=8)),
-        )
-        return storage.create(short_url_in)
 
     def test_update(self) -> None:
         short_url_update = ShortUrlUpdate(**self.short_url.model_dump())
@@ -81,3 +78,43 @@ class ShortUrlStorageUpdateTestCase(TestCase):
             short_url_partial_update.description,
             partial_updated_short_url.description,
         )
+
+
+class ShortUrlStorageGetTestCase(TestCase):
+
+    SHORT_URLS_COUNTS: Final[int] = 3
+    short_urls: ClassVar[list[ShortUrl]] = []
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.short_urls = [create_short_url() for _ in range(cls.SHORT_URLS_COUNTS)]
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        for short_url in cls.short_urls:
+            storage.delete(short_url)
+
+    def test_get_list(self) -> None:
+        short_urls = storage.get()
+        slug_in_db = {si.slug for si in short_urls}
+
+        expected_slug = {si.slug for si in self.short_urls}
+
+        expected_dif: set[ShortUrl] = set()
+        real_dif = expected_slug - slug_in_db
+        self.assertEqual(
+            expected_dif,
+            real_dif,
+        )
+
+    def test_get_by_slug(self) -> None:
+        for short_url in self.short_urls:
+            with self.subTest(
+                slug=short_url.slug,
+                msg=f"Validate can get slug {short_url.slug!r}",
+            ):
+                db_short_url = storage.get_by_slug(short_url.slug)
+                self.assertEqual(
+                    short_url,
+                    db_short_url,
+                )
