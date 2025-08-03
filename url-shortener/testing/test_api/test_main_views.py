b82a540f8d7222556def7370e7d9adf06c015b8f
GlebKostousov <kostousjr@gmail.com>
2025-08-03 22:49:15 +0300

    Написал параметрезированный тест

--- url-shortener/testing/test_api/test_main_views.py
+++ url-shortener/testing/test_api/test_main_views.py
@@ -1,5 +1,6 @@
 from fastapi import status
 from fastapi.testclient import TestClient
+from pytest import mark
 
 from main import app
 
@@ -9,6 +10,24 @@ client = TestClient(app=app)
 def test_root_view() -> None:
     # TODO: fake date
     name = "John"
+    query = {"name": name}
+    response = client.get("/", params=query)
+    assert response.status_code == status.HTTP_200_OK, response.text
+    response_data = response.json()
+    expected_message = f"Hello {name}!"
+    assert response_data["message"] == expected_message, response_data
+
+
+@mark.parametrize(
+    "name",
+    [
+        "john",
+        "",
+        "!@#$",
+        "Jpgm Smith",
+    ],
+)
+def test_root_view_custom_name(name: str) -> None:
     query = {"name": name}
     response = client.get("/", params=query)
     assert response.status_code == status.HTTP_200_OK, response.text
