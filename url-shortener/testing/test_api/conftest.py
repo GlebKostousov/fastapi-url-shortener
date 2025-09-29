c7856b4f6605b0353f14ef61f9a0fdc3af686772
GlebKostousov <kostousohr@gmail.com>
2025-09-29 12:39:55 +0300

    Создал фикстуры для написания тестов требующих авторизацию

--- url-shortener/testing/test_api/conftest.py
+++ url-shortener/testing/test_api/conftest.py
@@ -3,6 +3,7 @@ from collections.abc import Generator
 import pytest
 from fastapi.testclient import TestClient
 
+from api.api_v1.auth.services import redis_tokens
 from main import app
 
 
@@ -10,3 +11,17 @@ from main import app
 def client() -> Generator[TestClient]:
     with TestClient(app=app) as test_client:
         yield test_client
+
+
+@pytest.fixture(scope="module")
+def auth_token() -> Generator[str]:
+    token = redis_tokens.generate_and_save_token()
+    yield token
+    redis_tokens.delete_token(token)
+
+
+@pytest.fixture(scope="module")
+def auth_client(auth_token: str) -> Generator[TestClient]:
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    with TestClient(app=app, headers=headers) as client:
+        yield client
