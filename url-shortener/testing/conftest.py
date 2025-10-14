d11bd1e5b23b6a4becafceb3600802860f8d0b8f
GlebKostousov <kostousjr@gmail.com>
2025-10-14 09:35:29 +0300

    Сделали рефакторинг функций, чтобы можно было передавать description для создания short_url

--- url-shortener/testing/conftest.py
+++ url-shortener/testing/conftest.py
@@ -9,40 +9,55 @@ from pydantic import AnyHttpUrl
 from api.api_v1.short_urls.crud import storage
 from schemas.short_url import ShortUrl, ShortUrlCreate
 
+DESCRIPTION_FOR_TEST = "A short url"
+
 if getenv("TESTING") != "1":
     pytest.exit(
         "Environment is not ready for testing",
     )
 
 
-def build_short_url_create(slug: str) -> ShortUrlCreate:
+def build_short_url_create(
+    slug: str,
+    description: str = DESCRIPTION_FOR_TEST,
+) -> ShortUrlCreate:
+
     return ShortUrlCreate(
         slug=slug,
-        description="A short url",
+        description=description,
         target_url=AnyHttpUrl("https://example.com"),
     )
 
 
-def build_short_url_create_random_slug() -> ShortUrlCreate:
-    return ShortUrlCreate(
-        slug="".join(
-            random.choices(
-                string.ascii_letters,
-                k=8,
-            ),
+def _generate_slug() -> str:
+    return "".join(
+        random.choices(
+            string.ascii_letters,
+            k=8,
         ),
-        description="A short url",
+    )
+
+
+def build_short_url_create_random_slug(
+    description: str = DESCRIPTION_FOR_TEST,
+) -> ShortUrlCreate:
+    return ShortUrlCreate(
+        slug=_generate_slug(),
+        description=description,
         target_url=AnyHttpUrl("https://example.com"),
     )
 
 
-def create_short_url(slug: str) -> ShortUrl:
-    short_url_in = build_short_url_create(slug)
+def create_short_url(
+    slug: str,
+    description: str = DESCRIPTION_FOR_TEST,
+) -> ShortUrl:
+    short_url_in = build_short_url_create(slug=slug, description=description)
     return storage.create(short_url_in)
 
 
-def create_short_url_random_slug() -> ShortUrl:
-    short_url_in = build_short_url_create_random_slug()
+def create_short_url_random_slug(description: str = DESCRIPTION_FOR_TEST) -> ShortUrl:
+    short_url_in = build_short_url_create_random_slug(description=description)
     return storage.create(short_url_in)
 
 
