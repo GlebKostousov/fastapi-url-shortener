5e399d2f827de4200b986b491573d2fc57c747a5
GlebKostousov <kostousjr@gmail.com>
2025-11-06 09:58:37 +0300

    ci(python-checks): break type fot test

--- url-shortener/api/api_v1/auth/services/redis_users_helper.py
+++ url-shortener/api/api_v1/auth/services/redis_users_helper.py
@@ -1,7 +1,6 @@
 __all__ = ("redis_users",)
 
 import logging
-from typing import cast
 
 from redis import Redis
 
@@ -33,7 +32,7 @@ class UserTokensHelper(AbstractUsersHelper):
         if not self.redis_users.exists(username):
             return None
 
-        return cast("str | None", self.redis_users.get(name=username))
+        return self.redis_users.get(name=username)
 
     def add_user(self, username: str, password: str) -> None:
         self.redis_users.set(name=username, value=password)
