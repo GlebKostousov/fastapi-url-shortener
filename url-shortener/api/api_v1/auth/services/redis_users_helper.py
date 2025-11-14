34c3cd981bb8620cf1ed49e36ee0563db46fd28f
GlebKostousov <kostousjr@gmail.com>
2025-11-14 11:10:04 +0300

    refactor(redis-config): refactor redis config data to pydentic-settings

--- url-shortener/api/api_v1/auth/services/redis_users_helper.py
+++ url-shortener/api/api_v1/auth/services/redis_users_helper.py
@@ -1,3 +1,5 @@
+"""Module for helps with working with Redis db with username and password"""
+
 __all__ = ("redis_users",)
 
 import logging
@@ -6,13 +8,14 @@ from typing import cast
 from redis import Redis
 
 from api.api_v1.auth.services.users_helper import AbstractUsersHelper
-from core import config
 from core.config import settings
 
 log = logging.getLogger(__name__)
 
 
 class UserTokensHelper(AbstractUsersHelper):
+    """Implementation of the AbstractUsersHelper interface for the
+    login-password combination"""
 
     def __init__(
         self,
@@ -22,6 +25,15 @@ class UserTokensHelper(AbstractUsersHelper):
         *,
         decode_responses: bool = False,
     ) -> None:
+        """
+        init class
+
+        Args:
+            host (str): host url
+            port (int): port to connect
+            db (int): number of database in redis client
+            decode_responses (bool): auto decode responses to UTF-8. If False - bytes
+        """
 
         self.redis_users = Redis(
             host=host,
@@ -31,20 +43,38 @@ class UserTokensHelper(AbstractUsersHelper):
         )
 
     def get_user_password(self, username: str) -> str | None:
+        """
+        Get password from db by username
+
+        Args:
+            username (str): username for getting password
+
+        Returns:
+            str | None: If username in db - return password else None
+
+        """
         if not self.redis_users.exists(username):
             return None
 
         return cast("str | None", self.redis_users.get(name=username))
 
     def add_user(self, username: str, password: str) -> None:
-        self.redis_users.set(name=username, value=password)
+        """
+        Adds user and password to the database
 
-    # validate_user_password используй для проверки пароля
+        Args:
+            username (str): login
+            password (str): password
+
+        Returns:
+
+        """
+        self.redis_users.set(name=username, value=password)
 
 
 redis_users = UserTokensHelper(
     host=settings.redis.connection.host,
     port=settings.redis.connection.port,
-    db=config.REDIS_DB_USERS,
+    db=settings.redis.db.users,
     decode_responses=True,
 )
--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -12,7 +12,6 @@ import logging
 from pydantic import BaseModel
 from redis import Redis
 
-from core import config
 from core.config import settings
 from schemas.short_url import (
     ShortUrl,
@@ -26,21 +25,17 @@ log = logging.getLogger(__name__)
 redis_short_urls = Redis(
     host=settings.redis.connection.host,
     port=settings.redis.connection.port,
-    db=config.REDIS_DB_SHORT_URLS,
+    db=settings.redis.db.short_urls,
     decode_responses=True,
 )
 
 
 class ShortUrlBaseError(Exception):
-    """
-    Base exception for short url CRUD actions
-    """
+    """Base exception for short url CRUD actions"""
 
 
 class ShortUrlAlreadyExistsError(ShortUrlBaseError):
-    """
-    Raised when short url already exists
-    """
+    """Raised when short url already exists"""
 
     def __init__(self, slug: str) -> None:
         self.slug = slug
@@ -50,8 +45,17 @@ class ShortUrlStorage(BaseModel):
 
     @classmethod
     def save_state(cls, short_url: ShortUrl) -> None:
+        """
+        Saves short url data to db
+
+        Args:
+            short_url (ShortUrl): data to be saved
+
+        Returns:
+
+        """
         redis_short_urls.hset(
-            name=config.REDIS_SHORT_URLS_HASH_NAME,
+            name=settings.redis.namespace.hash_names.short_url,
             key=short_url.slug,
             value=short_url.model_dump_json(),
         )
@@ -59,15 +63,34 @@ class ShortUrlStorage(BaseModel):
 
     @classmethod
     def get(cls) -> list[ShortUrl]:
+        """
+        Get all short urls from db
+
+        Returns:
+            list[ShortUrl]: list of all short urls
+
+        """
         return [
             ShortUrl.model_validate_json(value)
-            for value in redis_short_urls.hvals(name=config.REDIS_SHORT_URLS_HASH_NAME)
+            for value in redis_short_urls.hvals(
+                name=settings.redis.namespace.hash_names.short_url,
+            )
         ]
 
     @classmethod
     def get_by_slug(cls, slug: str) -> ShortUrl | None:
+        """
+        Get short url by slug from db
+
+        Args:
+            slug (str): short url slug to find element in db
+
+        Returns:
+            ShortUrl | None. if short url exists with slug, else None
+
+        """
         if short_url_json := redis_short_urls.hget(
-            name=config.REDIS_SHORT_URLS_HASH_NAME,
+            name=settings.redis.namespace.hash_names.short_url,
             key=slug,
         ):
             return ShortUrl.model_validate_json(short_url_json)
@@ -76,30 +99,78 @@ class ShortUrlStorage(BaseModel):
 
     @classmethod
     def exists(cls, slug: str) -> bool:
+        """
+        Check existence of short url in db
+
+        Args:
+            slug (str): short url slug to check
+
+        Returns:
+            bool. True if exists, else False
+
+        """
         return bool(
             redis_short_urls.hexists(
-                name=config.REDIS_SHORT_URLS_HASH_NAME,
+                name=settings.redis.namespace.hash_names.short_url,
                 key=slug,
             ),
         )
 
     @classmethod
     def delete_by_slug(cls, slug: str) -> None:
-        redis_short_urls.hdel(config.REDIS_SHORT_URLS_HASH_NAME, slug)
+        """
+        Delete short url by slug
+
+        Args:
+            slug (str): slug of short url
+
+        Returns:
+
+        """
+        redis_short_urls.hdel(settings.redis.namespace.hash_names.short_url, slug)
 
     def create(self, short_url_in: ShortUrlCreate) -> ShortUrl:
+        """
+        Create short url
+
+        Args:
+            short_url_in (ShortUrlCreate): data to create short url
+
+        Returns:
+            ShortUrl - created data from db
+
+        """
         short_url: ShortUrl = ShortUrl(**short_url_in.model_dump())
         self.save_state(short_url)
         log.info("Created short url %s", short_url)
         return short_url
 
     def create_of_raise_if_exists(self, short_url_in: ShortUrlCreate) -> ShortUrl:
+        """
+        Check existence of short url. If exists, raise ShortUrlAlreadyExistsError,
+        else create short url
+
+        Args:
+            short_url_in (ShortUrlCreate): data from front to create short url
+
+        Returns:
+
+        """
         if not self.exists(short_url_in.slug):
             return self.create(short_url_in)
 
         raise ShortUrlAlreadyExistsError(short_url_in.slug)
 
     def delete(self, short_url_in: ShortUrl) -> None:
+        """
+        Delete short url
+
+        Args:
+            short_url_in (ShortUrl): Short url from db to delete
+
+        Returns:
+
+        """
         self.delete_by_slug(slug=short_url_in.slug)
 
     def update(
@@ -107,6 +178,17 @@ class ShortUrlStorage(BaseModel):
         short_url: ShortUrl,
         short_url_in: ShortUrlUpdate,
     ) -> ShortUrl:
+        """
+        Update short url
+
+        Args:
+            short_url (ShortUrl): Short url from db to update
+            short_url_in (ShortUrlUpdate): data from front to update short url
+
+        Returns:
+            ShortUrl - updated data from db
+
+        """
         for field, value in short_url_in:
             setattr(short_url, field, value)
         self.save_state(short_url)
@@ -117,6 +199,17 @@ class ShortUrlStorage(BaseModel):
         short_url: ShortUrl,
         short_url_in: ShortUrlPartialUpdate,
     ) -> ShortUrl:
+        """
+        Partially update short url
+
+        Args:
+            short_url (ShortUrl): Short url from db to update
+            short_url_in (ShortUrlPartialUpdate): data from front to update short url
+
+        Returns:
+            ShortUrl - updated data from db
+
+        """
         for field, value in short_url_in.model_dump(exclude_unset=True).items():
             setattr(short_url, field, value)
 
