54fbfee53d88ab07c076fa13dec7739aa7c7c383
GlebKostousov <kostousjr@gmail.com>
2025-11-24 11:34:41 +0300

    feat(short-urls-list): add context parser

--- url-shortener/views/short_urls/list_views.py
+++ url-shortener/views/short_urls/list_views.py
@@ -1,9 +1,13 @@
-from typing import Any
+from typing import TYPE_CHECKING
 
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
+from dependencies.short_urls import GetShortUrlsStorage
 from templating import templates
+
+if TYPE_CHECKING:
+    from schemas.short_url import ShortUrl
 
 router = APIRouter()
 
@@ -13,9 +17,12 @@ router = APIRouter()
     name="short-urls:list",
     response_class=HTMLResponse,
 )
-def list_view(request: Request) -> HTMLResponse:
-    context: dict[str, Any] = {}
-    short_urls = []
+def list_view(
+    request: Request,
+    storage: GetShortUrlsStorage,
+) -> HTMLResponse:
+    context: dict[str, list[ShortUrl]] = {}
+    short_urls = storage.get()
     context.update(short_urls=short_urls)
     return templates.TemplateResponse(
         request=request,
