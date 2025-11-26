ca737bf329e8c60538e651ab7c74b7f4f61b6089
GlebKostousov <kostousjr@gmail.com>
2025-11-26 11:50:46 +0300

    feat(short-urls-create): Handled the ShortUrlAlreadyExistsError error while filling out the form

--- url-shortener/views/short_urls/create_views.py
+++ url-shortener/views/short_urls/create_views.py
@@ -1,3 +1,4 @@
+from logging import getLogger
 from typing import Annotated, Any
 
 from fastapi import APIRouter, Form, status
@@ -6,8 +7,10 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 
 from dependencies.short_urls import GetShortUrlsStorage
 from schemas.short_url import ShortUrlCreate
+from storage.short_url.exceptions import ShortUrlAlreadyExistsError
 from templating import templates
 
+logger = getLogger(__name__)
 router = APIRouter(
     prefix="/create",
 )
@@ -34,6 +37,7 @@ def get_page_crate_short_url(
 @router.post(
     path="/",
     name="short-urls:create",
+    response_model=None,
 )
 def create_short_url(
     short_url_create: Annotated[
@@ -42,11 +46,32 @@ def create_short_url(
     ],
     storage: GetShortUrlsStorage,
     request: Request,
-) -> RedirectResponse:
-    storage.create_of_raise_if_exists(
-        short_url_in=short_url_create,
+) -> RedirectResponse | HTMLResponse:
+    try:
+        storage.create_of_raise_if_exists(
+            short_url_in=short_url_create,
+        )
+
+    except ShortUrlAlreadyExistsError:
+        errors = {
+            "slug": f"Short url with slug {short_url_create.slug!r} already exists.",
+        }
+    else:
+        return RedirectResponse(
+            url=request.url_for("short-urls:list"),
+            status_code=status.HTTP_303_SEE_OTHER,
+        )
+
+    context: dict[str, Any] = {}
+    context.update(
+        model_schema=ShortUrlCreate.model_json_schema(),
+        errors=errors,
+        form_validated=True,
+        form_data=short_url_create,
     )
-    return RedirectResponse(
-        url=request.url_for("short-urls:list"),
-        status_code=status.HTTP_303_SEE_OTHER,
+    return templates.TemplateResponse(
+        name="short_urls/create.html",
+        request=request,
+        context=context,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
     )
