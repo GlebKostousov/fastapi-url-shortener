a5fe348edc9b142d0d4054b1d8e3535bb868c59a
GlebKostousov <kostousjr@gmail.com>
2025-11-28 09:37:03 +0300

    build(project): new dependency module

--- url-shortener/views/short_urls/__init__.py
+++ url-shortener/views/short_urls/__init__.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from dependencies.auth import user_basic_auth_required_for_unsafe_methods
 from views.short_urls.create_views import router as create_router
 from views.short_urls.list_views import router as list_router
 
 router = APIRouter(
     prefix="/short-urls",
     tags=["Short URLs views"],
+    dependencies=[Depends(user_basic_auth_required_for_unsafe_methods)],
 )
 router.include_router(list_router)
 router.include_router(create_router)
