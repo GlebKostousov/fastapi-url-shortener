c3cbed94536ae8b522d960caedd8ccdfa7dbec85
GlebKostousov <kostousjr@gmail.com>
2025-12-04 01:42:38 +0300

    front/delete (#37)
    * feat(delete): add modal window for delete
    
    * feat(delete): activate delete button
    
    * feat(list-view): integrate modal window in list-view
    
    * feat(delete): start add new view to delete
    
    * feat(delete): include new router
    
    * feat(delete): add form to delete button in modal window
    
    * feat(delete): add new context param to model window
    
    * feat(delete): finish delete view

---
+++ url-shortener/views/short_urls/delete_views.py
@@ -0,0 +1,31 @@
+from logging import getLogger
+
+from fastapi import APIRouter
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+
+from dependencies.short_urls import GetShortUrlsStorage, ShortUrlBySlug
+
+logger = getLogger(__name__)
+router = APIRouter(
+    prefix="/{slug}/delete",
+)
+
+
+@router.post(
+    path="/",
+    name="short-urls:delete",
+    response_model=None,
+)
+def delete_short_url(
+    request: Request,
+    storage: GetShortUrlsStorage,
+    short_url: ShortUrlBySlug,
+) -> RedirectResponse:
+    storage.delete(short_url_in=short_url)
+
+    return RedirectResponse(
+        url=request.url_for("short-urls:list"),
+        status_code=status.HTTP_303_SEE_OTHER,
+    )
